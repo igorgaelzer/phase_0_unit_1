a4c5f481dae2e49ab52c8923530a24972632137d
Igor Gaelzer <igor@nordweg.com>
2014-05-31 16:50:56 -0300

    added reflection

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,147 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself.
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
- 
- 
- 
- 
- 
- 
- 
- 
+/*
+
+RESCUE MISSION
+this.moveDown();
+this.moveDown();
+this.moveRight();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+this.moveRight();
+this.moveDown();
+this.attackNearbyEnemy();
+
+
+GRAB THE MUSHROOM
+this.moveUp();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+this.attackNearbyEnemy();
+
+
+DRINK ME
+this.moveRight();
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+
+
+TAUNT THE GUARDS
+this.moveRight();
+this.bustDownDoor();
+this.moveRight();
+this.say("Hey there!");
+this.moveLeft();
+this.moveLeft();
+this.say("Attack!");
+this.moveRight();
+this.say("Follow me.");
+this.moveRight();
+this.moveRight();
+this.moveUp();
+this.moveUp();
+this.moveRight();
+
+
+IT'S A TRAP
+this.say("Hey!");
+this.say("Hey silly head!");
+this.say("Cmon you big bully!");
+this.say("I'll show you!");
+
+BREAK THE PRISON
+this.moveXY(50, 16);
+this.moveXY(63, 20);
+this.moveXY(70, 10);  // This is a safe spot.
+this.say("I can lure them in here.");
+// Run your soldier out where the ogres can hear you
+this.moveXY(54,34);
+// Say something!
+this.say("Hey you mean heads!");
+// Then run back behind the arrow towers for safety
+this.moveXY(70, 10);
+
+
+COMMANDING FOLLOWERS
+this.moveXY(49, 66);
+this.moveXY(60, 63);
+this.moveXY(75, 63);
+this.say("Hail, friends!");
+// Saying anything with "follow" in it will get nearby soldiers
+// to follow you
+this.say("Follow me comrads!");
+// Saying anything with "attack" will send nearby troops into
+// a battle mode.
+this.say("It's time to ATTACK!");
+// Make sure Tharin is safe!
+this.moveXY(66, 44);
+this.say("Come get some!");
+this.moveXY(75, 63);
+
+
+MOBILE ARTILLERY
+this.moveXY(30, 26);  // Move into range
+this.attackXY(46, 5);  // Shoot once in the middle of the ogres
+// If they survive a hit, ogres retaliate!
+// Larger ogres take more than one hit...
+// So, position and aim carefully.
+this.moveXY(44,43);
+this.attackXY(50,63);
+this.attackXY(48,56);
+this.attackXY(69,54);
+this.attackXY(57,47);
+
+*/ 
+  
 // Reflection:
-// Write your reflection here.
+
+/*
+
+What is this referring to? Think programming-wise rather than in the terms of the game.
+It allows an object to be called outside of a function.
+
+What does the () do in JavaScript?
+It says that the word before it is a function and it holds it's properties.
+
+What is the point of the semicolons?
+It tells the computer that is the end of that command.
+
+
+What parts of your strategy worked? What problems did you face?
+It was hard to figure out what the game wanted you to do, not the code or logic itself.
+
+
+What questions did you have while coding? What resources did you find to help you answer them?
+n/a
+
+
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+I understood it all.
+
+
+Did you learn any new skills or tricks?
+No.
+
+
+How confident are you with each of the Learning Competencies?
+Ok.
+
+
+Which parts of the challenge did you enjoy?
+I did not enjoy this challenge.
+
+
+Which parts of the challenge did you find tedious?
+There was so much to do this week and this is so basic and time consuming that I got frustrated having to do this while there is so much waiting to be done yet.
+
+*/

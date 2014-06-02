90256f5c60375a745607b6f731997844c94772e6
Igor Gaelzer <igor@nordweg.com>
2014-06-01 21:36:10 -0300

    added my solution

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -2,34 +2,37 @@
 
 // Your mission description:
 // 
-// 
+// Get to the princess in the other side of the hall and kill the monster in the middle
 // 
 
 // Pseudocode
 // 
-// 
-// 
-// 
+// Move right 2x
+// Kill Monster
+// Move right 2x
 // 
 
 // Initial Code
 
-
-
-
+this.moveRight();
+this.moveRight();
+this.killMonster();
+this.moveRight();
+this.moveRight();
 
 
 // Refactored Code
 
-
-
-
-
+this.moveRight();
+this.moveRight();
+this.killMonster();
+this.moveRight();
+this.moveRight();
 
 // Reflection
 // 
-// 
-// 
+// I found it a pain having to do this. I did not understand at all what to do and, especially, WHY??
+// Time is so limited and I feel like it should be used with something relevant. Maybe I just don't get it, but I really did try.
 // 
 // 
 // 
